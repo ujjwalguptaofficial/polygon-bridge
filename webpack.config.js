ad3a12a6b741776f3c28f4b621fab8b5697e9bb6
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-09-05 14:59:59 +0530

    generate files in docs

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'bin/')
+    path: path.resolve(__dirname, 'docs/')
   },
   plugins: [
     new HtmlWebPackPlugin({
